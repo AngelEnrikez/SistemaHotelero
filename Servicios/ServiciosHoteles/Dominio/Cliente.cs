433e3000ad712c336954150bd82b0db51184c5ae
jpascual728@gmail.com <jpascual728@gmail.com>
2015-05-21 17:24:24 -0500

    clientes

--- Servicios/ServiciosHoteles/Dominio/Cliente.cs
+++ Servicios/ServiciosHoteles/Dominio/Cliente.cs
@@ -13,5 +13,15 @@ namespace ServiciosHoteles.Dominio
         public int Codigo { get; set; }
         [DataMember]
         public string Nombre { get; set; }
+        [DataMember]
+        public string ApellidoPaterno { get; set; }
+        [DataMember]
+        public int DNI { get; set; }
+        [DataMember]
+        public string Telefono { get; set; }
+        [DataMember]
+        public string Email { get; set; }
+
+
     }
 }
