024bcad55845f040d929e2bea4d9e21e1b076cee
ibaraybar <ibaraybard@gmail.com>
2015-05-23 07:32:31 -0500

    Creacion de servicio CheckOut

---
+++ Servicios/ServiciosHoteles/CheckOut.svc.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ServiciosHoteles
+{
+    public class CheckOut : ICheckOut
+    {
+        public string RegistrarCheckOut(int idReserva, string comentario)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Servicios/ServiciosHoteles/ICheckOut.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ServiciosHoteles
+{
+    [ServiceContract]
+    public interface ICheckOut
+    {
+        [OperationContract]
+        string RegistrarCheckOut(int idReserva, string comentario);
+    }
+}
