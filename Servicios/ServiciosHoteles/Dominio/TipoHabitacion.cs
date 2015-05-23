a37a054f796812b6aaecc0ff57edfafd28d0adea
ibaraybar <ibaraybard@gmail.com>
2015-05-23 06:25:23 -0500

    Creación de entidad TipoHabitacion

---
+++ Servicios/ServiciosHoteles/Dominio/TipoHabitacion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace ServiciosHoteles.Dominio
+{
+    [DataContract]
+    public class TipoHabitacion
+    {
+        [DataMember]
+        public int IdTipoHabitacion { get; set; }
+        [DataMember]
+        public string Descripcion { get; set; }
+        [DataMember]
+        public decimal Tarifa { get; set; }
+    }
+}
