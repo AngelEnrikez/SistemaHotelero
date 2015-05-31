ea9907cc6a2761aa2a2d2cf6bc53df2d9bdc7f78
jpascual728@gmail.com <jpascual728@gmail.com>
2015-05-31 16:48:34 -0500

    a

---
+++ Servicios/ServiciosHoteles/Dominio/Habitacion.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+
+namespace ServiciosHoteles.Dominio
+{
+    [DataContract]
+    public class Habitacion
+    {
+        [DataMember]
+        public int idHabitacion { get; set; }
+
+        [DataMember]
+        public int idTipoHabitacion { get; set; }
+        [DataMember]
+        public int numero { get; set; }
+        [DataMember]
+        public int piso { get; set; }
+        [DataMember]
+        public string descripcion { get; set; }
+
+
+    }
+}
---
+++ Servicios/ServiciosHoteles/Dominio/Servicio.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace ServiciosHoteles.Dominio
+{
+
+    [DataContract]
+    public class Servicio
+    {
+
+        [DataMember]
+        public int idServicio { get; set; }
+
+        [DataMember]
+        public string descripcion { get; set; }
+
+        [DataMember]
+        public decimal tarifa { get; set; }
+    }
+}
