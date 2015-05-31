a9bb228a191528a70a649ac55f50e0a8e8ca2cba
jpascual728@gmail.com <jpascual728@gmail.com>
2015-05-31 16:05:34 -0500

    implementacion

---
+++ Servicios/ServiciosHoteles/Util/Constantes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace ServiciosHoteles.Util
+{
+
+     [DataContract]
+    public class Constantes
+    {
+
+       [DataMember]
+        enum Days { Sun, Mon, Tue, Wed, Thu, Fri, Sat };
+
+    }
+}
