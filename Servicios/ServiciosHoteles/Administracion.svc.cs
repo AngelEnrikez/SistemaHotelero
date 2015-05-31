f3af6bd8fd9ca0a4289b61b0a309156bee86c4b6
jpascual728@gmail.com <jpascual728@gmail.com>
2015-05-31 15:45:12 -0500

    creacion de clases para listado

---
+++ Servicios/ServiciosHoteles/Administracion.svc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ServiciosHoteles
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Administracion" en el código, en svc y en el archivo de configuración a la vez.
+    // NOTA: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione Administracion.svc o Administracion.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class Administracion : IAdministracion
+    {
+        public void DoWork()
+        {
+        }
+    }
+}
---
+++ Servicios/ServiciosHoteles/Dominio/Parametro.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ServiciosHoteles.Dominio
+{
+    public class Parametro
+    {
+
+        public int idConfiguracion { get; set; }
+
+        public string claveConfig { get; set; }
+
+        public string valorConfig { get; set; }
+
+    }
+}
