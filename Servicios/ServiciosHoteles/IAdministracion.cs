0877963de5d887923d694922fcf815aa1f585665
jpascual728@gmail.com <jpascual728@gmail.com>
2015-05-31 16:46:28 -0500

    cambios

---
+++ Servicios/ServiciosHoteles/IAdministracion.cs
@@ -0,0 +1,39 @@
+﻿using ServiciosHoteles.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using ServiciosHoteles.Util;
+
+namespace ServiciosHoteles
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IAdministracion" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface IAdministracion
+    {
+        [OperationContract]
+        List<Cliente> AdminClientes(Constantes valor);
+
+        [OperationContract]
+        List<TipoDocumento> AdminTDocumentos();
+
+        [OperationContract]
+        List<Habitacion> AdminHabitaciones();
+
+        [OperationContract]
+        List<TipoHabitacion> AdminTHabitaciones();
+
+        [OperationContract]
+        List<Pais> AdminPaises();
+
+        [OperationContract]
+        List<Servicio> AdminServicios();
+
+        [OperationContract]
+        List<Parametro> AdminParametros();
+
+
+    }
+}
