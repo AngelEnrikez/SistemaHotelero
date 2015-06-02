eb02f8d891bc1fe5b09e135521cb525f50bfd420
ibaraybar <ibaraybard@gmail.com>
2015-06-02 13:42:04 -0500

    Implementacion de CheckOut

--- Servicios/ServiciosHoteles/CheckOut.svc.cs
+++ Servicios/ServiciosHoteles/CheckOut.svc.cs
@@ -1,17 +1,38 @@
-﻿using System;
+﻿using ServiciosHoteles.Dominio;
+using ServiciosHoteles.Util;
+using ServiciosHoteles.WSReservas;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Runtime.Serialization;
 using System.ServiceModel;
+using System.ServiceModel.Web;
 using System.Text;
 
 namespace ServiciosHoteles
 {
     public class CheckOut : ICheckOut
     {
-        public string RegistrarCheckOut(int idReserva, string comentario)
+        public Reserva RegistrarCheckOut(Reserva reservaCheckedOut)
         {
-            throw new NotImplementedException();
+            //Regla de Negocio: La Reserva debe estar en estado CheckedIn
+            if (reservaCheckedOut.Estado != (int)EstadosReserva.CheckedIn)
+            {
+                throw new WebFaultException<string>(
+                    "La Reserva debe tener estado CheckedIn", HttpStatusCode.PreconditionFailed);
+            }
+            //Regla de Negocio: La Cuenta debe estar cerrada para poder registrar el CheckOut
+            else if (reservaCheckedOut.EstadoCuenta == false)
+            {
+                throw new WebFaultException<string>(
+                    "La Reserva debe tener la Cuenta Cerrada/Confirmada", HttpStatusCode.PreconditionFailed);
+            }
+
+            ReservasClient proxy = new ReservasClient();
+            reservaCheckedOut.FechaHoraCheckout = DateTime.Now;
+            reservaCheckedOut.Estado = (int)EstadosReserva.CheckedOut;
+            return proxy.ModificarReserva(reservaCheckedOut);
         }
     }
 }
