98d387627afd6a64eb256224228330528107059d
ibaraybar <ibaraybard@gmail.com>
2015-06-02 11:15:00 -0500

    Creacion Prueba Unitaria CheckOut

---
+++ HotelUnitTests/UnitTestCheckOut.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HotelUnitTests
+{
+    /// <summary>
+    /// Descripción resumida de UnitTestCheckOut
+    /// </summary>
+    [TestClass]
+    public class UnitTestCheckOut
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //
+            // TODO: Agregar aquí la lógica de las pruebas
+            //
+        }
+    }
+}
