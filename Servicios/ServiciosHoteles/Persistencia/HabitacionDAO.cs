7b923d7d0045581657cb3e9e74b2662ab2673076
jpascual728@gmail.com <jpascual728@gmail.com>
2015-06-10 17:34:18 -0500

    DAO's

--- Servicios/ServiciosHoteles/Persistencia/HabitacionDAO.cs
+++ Servicios/ServiciosHoteles/Persistencia/HabitacionDAO.cs
@@ -11,5 +11,16 @@ namespace ServiciosHoteles.Persistencia
 {
     public class HabitacionDAO : BaseDAO<Habitacion, int>
     {
+
+
+        public ICollection<Habitacion> Buscar(int codigo_habitacion)
+        {
+            using (ISession sesion = NHibernateHelper.ObtenerSesion())
+            {
+                ICriteria busqueda = sesion.CreateCriteria(typeof(Habitacion));
+
+                return busqueda.List<Habitacion>();
+            }
+        }
     }
 }
