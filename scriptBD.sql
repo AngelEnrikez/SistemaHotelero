9dcb36550a5bf62dcb1bbef026de085ef8e30abb
ibaraybar <ibaraybard@gmail.com>
2015-06-10 20:18:38 -0500

    cambios en la BD

--- scriptBD.sql
+++ scriptBD.sql
@@ -1,79 +1,8 @@
-﻿USE [master]
-GO
-/****** Object:  Database [BDHoteles]    Script Date: 05/30/2015 22:26:16 ******/
-CREATE DATABASE [BDHoteles] ON  PRIMARY 
-( NAME = N'BDHoteles', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL10_50.MSSQLSERVER\MSSQL\DATA\BDHoteles.mdf' , SIZE = 3072KB , MAXSIZE = UNLIMITED, FILEGROWTH = 1024KB )
- LOG ON 
-( NAME = N'BDHoteles_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL10_50.MSSQLSERVER\MSSQL\DATA\BDHoteles_log.ldf' , SIZE = 1024KB , MAXSIZE = 2048GB , FILEGROWTH = 10%)
-GO
-ALTER DATABASE [BDHoteles] SET COMPATIBILITY_LEVEL = 100
-GO
-IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
-begin
-EXEC [BDHoteles].[dbo].[sp_fulltext_database] @action = 'enable'
-end
-GO
-ALTER DATABASE [BDHoteles] SET ANSI_NULL_DEFAULT OFF
-GO
-ALTER DATABASE [BDHoteles] SET ANSI_NULLS OFF
-GO
-ALTER DATABASE [BDHoteles] SET ANSI_PADDING OFF
-GO
-ALTER DATABASE [BDHoteles] SET ANSI_WARNINGS OFF
-GO
-ALTER DATABASE [BDHoteles] SET ARITHABORT OFF
-GO
-ALTER DATABASE [BDHoteles] SET AUTO_CLOSE OFF
-GO
-ALTER DATABASE [BDHoteles] SET AUTO_CREATE_STATISTICS ON
-GO
-ALTER DATABASE [BDHoteles] SET AUTO_SHRINK OFF
-GO
-ALTER DATABASE [BDHoteles] SET AUTO_UPDATE_STATISTICS ON
-GO
-ALTER DATABASE [BDHoteles] SET CURSOR_CLOSE_ON_COMMIT OFF
-GO
-ALTER DATABASE [BDHoteles] SET CURSOR_DEFAULT  GLOBAL
-GO
-ALTER DATABASE [BDHoteles] SET CONCAT_NULL_YIELDS_NULL OFF
-GO
-ALTER DATABASE [BDHoteles] SET NUMERIC_ROUNDABORT OFF
-GO
-ALTER DATABASE [BDHoteles] SET QUOTED_IDENTIFIER OFF
-GO
-ALTER DATABASE [BDHoteles] SET RECURSIVE_TRIGGERS OFF
-GO
-ALTER DATABASE [BDHoteles] SET  DISABLE_BROKER
-GO
-ALTER DATABASE [BDHoteles] SET AUTO_UPDATE_STATISTICS_ASYNC OFF
-GO
-ALTER DATABASE [BDHoteles] SET DATE_CORRELATION_OPTIMIZATION OFF
-GO
-ALTER DATABASE [BDHoteles] SET TRUSTWORTHY OFF
-GO
-ALTER DATABASE [BDHoteles] SET ALLOW_SNAPSHOT_ISOLATION OFF
-GO
-ALTER DATABASE [BDHoteles] SET PARAMETERIZATION SIMPLE
-GO
-ALTER DATABASE [BDHoteles] SET READ_COMMITTED_SNAPSHOT OFF
-GO
-ALTER DATABASE [BDHoteles] SET HONOR_BROKER_PRIORITY OFF
-GO
-ALTER DATABASE [BDHoteles] SET  READ_WRITE
-GO
-ALTER DATABASE [BDHoteles] SET RECOVERY FULL
-GO
-ALTER DATABASE [BDHoteles] SET  MULTI_USER
-GO
-ALTER DATABASE [BDHoteles] SET PAGE_VERIFY CHECKSUM
-GO
-ALTER DATABASE [BDHoteles] SET DB_CHAINING OFF
-GO
-EXEC sys.sp_db_vardecimal_storage_format N'BDHoteles', N'ON'
+﻿CREATE DATABASE [BDHoteles] 
 GO
 USE [BDHoteles]
 GO
-/****** Object:  Table [dbo].[t_Usuario]    Script Date: 05/30/2015 22:26:18 ******/
+/****** Object:  Table [dbo].[t_Usuario]    Script Date: 06/10/2015 20:15:51 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -92,7 +21,8 @@ CREATE TABLE [dbo].[t_Usuario](
 GO
 SET ANSI_PADDING OFF
 GO
-/****** Object:  Table [dbo].[t_TipoHabitacion]    Script Date: 05/30/2015 22:26:18 ******/
+INSERT [dbo].[t_Usuario] ([idUsuario], [usuario], [password]) VALUES (1, N'admin', N'admin')
+/****** Object:  Table [dbo].[t_TipoHabitacion]    Script Date: 06/10/2015 20:15:51 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -111,7 +41,11 @@ CREATE TABLE [dbo].[t_TipoHabitacion](
 GO
 SET ANSI_PADDING OFF
 GO
-/****** Object:  Table [dbo].[t_TipoDocumento]    Script Date: 05/30/2015 22:26:18 ******/
+INSERT [dbo].[t_TipoHabitacion] ([idTipoHabitacion], [descripcion], [tarifa]) VALUES (1, N'Simple', CAST(80.00 AS Decimal(14, 2)))
+INSERT [dbo].[t_TipoHabitacion] ([idTipoHabitacion], [descripcion], [tarifa]) VALUES (2, N'Doble', CAST(140.00 AS Decimal(14, 2)))
+INSERT [dbo].[t_TipoHabitacion] ([idTipoHabitacion], [descripcion], [tarifa]) VALUES (3, N'Triple', CAST(200.00 AS Decimal(14, 2)))
+INSERT [dbo].[t_TipoHabitacion] ([idTipoHabitacion], [descripcion], [tarifa]) VALUES (4, N'Matrimonial', CAST(210.00 AS Decimal(14, 2)))
+/****** Object:  Table [dbo].[t_TipoDocumento]    Script Date: 06/10/2015 20:15:51 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -129,7 +63,10 @@ CREATE TABLE [dbo].[t_TipoDocumento](
 GO
 SET ANSI_PADDING OFF
 GO
-/****** Object:  Table [dbo].[t_Servicio]    Script Date: 05/30/2015 22:26:18 ******/
+INSERT [dbo].[t_TipoDocumento] ([idTipoDocumento], [descripcion]) VALUES (1, N'DNI')
+INSERT [dbo].[t_TipoDocumento] ([idTipoDocumento], [descripcion]) VALUES (2, N'Pasaporte')
+INSERT [dbo].[t_TipoDocumento] ([idTipoDocumento], [descripcion]) VALUES (3, N'Carnet Extranjería')
+/****** Object:  Table [dbo].[t_Servicio]    Script Date: 06/10/2015 20:15:51 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -148,7 +85,12 @@ CREATE TABLE [dbo].[t_Servicio](
 GO
 SET ANSI_PADDING OFF
 GO
-/****** Object:  Table [dbo].[t_Pais]    Script Date: 05/30/2015 22:26:18 ******/
+INSERT [dbo].[t_Servicio] ([idServicio], [descripcion], [tarifa]) VALUES (1, N'Alojamiento', CAST(0.00 AS Decimal(14, 2)))
+INSERT [dbo].[t_Servicio] ([idServicio], [descripcion], [tarifa]) VALUES (2, N'Lavandería', CAST(20.00 AS Decimal(14, 2)))
+INSERT [dbo].[t_Servicio] ([idServicio], [descripcion], [tarifa]) VALUES (3, N'Restaurante', CAST(50.00 AS Decimal(14, 2)))
+INSERT [dbo].[t_Servicio] ([idServicio], [descripcion], [tarifa]) VALUES (4, N'Frigobar', CAST(0.00 AS Decimal(14, 2)))
+INSERT [dbo].[t_Servicio] ([idServicio], [descripcion], [tarifa]) VALUES (5, N'Transporte', CAST(30.00 AS Decimal(14, 2)))
+/****** Object:  Table [dbo].[t_Pais]    Script Date: 06/10/2015 20:15:51 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -166,7 +108,17 @@ CREATE TABLE [dbo].[t_Pais](
 GO
 SET ANSI_PADDING OFF
 GO
-/****** Object:  Table [dbo].[t_Configuracion]    Script Date: 05/30/2015 22:26:18 ******/
+INSERT [dbo].[t_Pais] ([idPais], [nombrePais]) VALUES (1, N'Perú')
+INSERT [dbo].[t_Pais] ([idPais], [nombrePais]) VALUES (2, N'Estados Unidos')
+INSERT [dbo].[t_Pais] ([idPais], [nombrePais]) VALUES (3, N'España')
+INSERT [dbo].[t_Pais] ([idPais], [nombrePais]) VALUES (4, N'Brasil')
+INSERT [dbo].[t_Pais] ([idPais], [nombrePais]) VALUES (5, N'Alemania')
+INSERT [dbo].[t_Pais] ([idPais], [nombrePais]) VALUES (6, N'Francia')
+INSERT [dbo].[t_Pais] ([idPais], [nombrePais]) VALUES (7, N'Chile')
+INSERT [dbo].[t_Pais] ([idPais], [nombrePais]) VALUES (8, N'Argentina')
+INSERT [dbo].[t_Pais] ([idPais], [nombrePais]) VALUES (9, N'Holanda')
+INSERT [dbo].[t_Pais] ([idPais], [nombrePais]) VALUES (10, N'Inglaterra')
+/****** Object:  Table [dbo].[t_Configuracion]    Script Date: 06/10/2015 20:15:51 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -185,7 +137,14 @@ CREATE TABLE [dbo].[t_Configuracion](
 GO
 SET ANSI_PADDING OFF
 GO
-/****** Object:  Table [dbo].[t_Cliente]    Script Date: 05/30/2015 22:26:18 ******/
+INSERT [dbo].[t_Configuracion] ([idConfiguracion], [claveConfig], [valorConfig]) VALUES (1, N'TMINCANCEL', N'48')
+INSERT [dbo].[t_Configuracion] ([idConfiguracion], [claveConfig], [valorConfig]) VALUES (2, N'MINOCUPABI', N'0.80')
+INSERT [dbo].[t_Configuracion] ([idConfiguracion], [claveConfig], [valorConfig]) VALUES (3, N'HRACHECKIN', N'15:00')
+INSERT [dbo].[t_Configuracion] ([idConfiguracion], [claveConfig], [valorConfig]) VALUES (4, N'HRACHEKOUT', N'12:00')
+INSERT [dbo].[t_Configuracion] ([idConfiguracion], [claveConfig], [valorConfig]) VALUES (5, N'IGV', N'0.18')
+INSERT [dbo].[t_Configuracion] ([idConfiguracion], [claveConfig], [valorConfig]) VALUES (6, N'SERIECOMP', N'005')
+INSERT [dbo].[t_Configuracion] ([idConfiguracion], [claveConfig], [valorConfig]) VALUES (7, N'ULTNROCOMP', N'120')
+/****** Object:  Table [dbo].[t_Cliente]    Script Date: 06/10/2015 20:15:51 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -210,7 +169,8 @@ CREATE TABLE [dbo].[t_Cliente](
 GO
 SET ANSI_PADDING OFF
 GO
-/****** Object:  Table [dbo].[t_Habitacion]    Script Date: 05/30/2015 22:26:18 ******/
+INSERT [dbo].[t_Cliente] ([idCliente], [nombre], [apellidoPaterno], [apellidoMaterno], [idTipoDocumento], [numeroDocumento], [email], [telefono], [idPais]) VALUES (1, N'Ivan', N'Baraybar', N'Delgado', 1, N'99999999', N'ibaraybard@gmail.com', N'5555555', 1)
+/****** Object:  Table [dbo].[t_Habitacion]    Script Date: 06/10/2015 20:15:51 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -231,7 +191,23 @@ CREATE TABLE [dbo].[t_Habitacion](
 GO
 SET ANSI_PADDING OFF
 GO
-/****** Object:  Table [dbo].[t_Reserva]    Script Date: 05/30/2015 22:26:18 ******/
+INSERT [dbo].[t_Habitacion] ([idHabitacion], [idTipoHabitacion], [numero], [piso], [descripcion]) VALUES (1, 1, 101, 1, N'Habitación Simple')
+INSERT [dbo].[t_Habitacion] ([idHabitacion], [idTipoHabitacion], [numero], [piso], [descripcion]) VALUES (2, 1, 102, 1, N'Habitación Simple')
+INSERT [dbo].[t_Habitacion] ([idHabitacion], [idTipoHabitacion], [numero], [piso], [descripcion]) VALUES (3, 2, 103, 1, N'Habitación Doble')
+INSERT [dbo].[t_Habitacion] ([idHabitacion], [idTipoHabitacion], [numero], [piso], [descripcion]) VALUES (4, 2, 104, 1, N'Habitación Doble')
+INSERT [dbo].[t_Habitacion] ([idHabitacion], [idTipoHabitacion], [numero], [piso], [descripcion]) VALUES (5, 2, 201, 2, N'Habitación Doble')
+INSERT [dbo].[t_Habitacion] ([idHabitacion], [idTipoHabitacion], [numero], [piso], [descripcion]) VALUES (6, 2, 202, 2, N'Habitación Doble')
+INSERT [dbo].[t_Habitacion] ([idHabitacion], [idTipoHabitacion], [numero], [piso], [descripcion]) VALUES (7, 1, 203, 2, N'Habitación Simple')
+INSERT [dbo].[t_Habitacion] ([idHabitacion], [idTipoHabitacion], [numero], [piso], [descripcion]) VALUES (8, 3, 204, 2, N'Habitación Triple')
+INSERT [dbo].[t_Habitacion] ([idHabitacion], [idTipoHabitacion], [numero], [piso], [descripcion]) VALUES (9, 2, 301, 3, N'Habitación Doble')
+INSERT [dbo].[t_Habitacion] ([idHabitacion], [idTipoHabitacion], [numero], [piso], [descripcion]) VALUES (10, 2, 302, 3, N'Habitación Doble')
+INSERT [dbo].[t_Habitacion] ([idHabitacion], [idTipoHabitacion], [numero], [piso], [descripcion]) VALUES (11, 1, 303, 3, N'Habitación Simple')
+INSERT [dbo].[t_Habitacion] ([idHabitacion], [idTipoHabitacion], [numero], [piso], [descripcion]) VALUES (12, 4, 304, 3, N'Habitación Matrimonial')
+INSERT [dbo].[t_Habitacion] ([idHabitacion], [idTipoHabitacion], [numero], [piso], [descripcion]) VALUES (13, 4, 401, 4, N'Habitación Matrimonial')
+INSERT [dbo].[t_Habitacion] ([idHabitacion], [idTipoHabitacion], [numero], [piso], [descripcion]) VALUES (14, 4, 402, 4, N'Habitación Matrimonial')
+INSERT [dbo].[t_Habitacion] ([idHabitacion], [idTipoHabitacion], [numero], [piso], [descripcion]) VALUES (15, 4, 403, 4, N'Habitación Matrimonial')
+INSERT [dbo].[t_Habitacion] ([idHabitacion], [idTipoHabitacion], [numero], [piso], [descripcion]) VALUES (16, 3, 404, 4, N'Habitación Triple')
+/****** Object:  Table [dbo].[t_Reserva]    Script Date: 06/10/2015 20:15:51 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -249,12 +225,12 @@ CREATE TABLE [dbo].[t_Reserva](
 	[fechaHoraCheckout] [datetime] NULL,
 	[comentarioCheckout] [varchar](250) NULL,
 	[codFormaPago] [char](2) NOT NULL,
-	[numeroTarjeta] [varchar](50) NOT NULL,
+	[numeroTarjeta] [varchar](50) NULL,
 	[titularTarjeta] [varchar](80) NULL,
 	[mesExpiraTarjeta] [int] NULL,
 	[anioExpiraTarjeta] [int] NULL,
 	[requerimientosEsp] [varchar](250) NULL,
-	[observaciones] [varchar](250) NULL ,
+	[observaciones] [varchar](250) NULL,
 	[estadoCuenta] [bit] NOT NULL,
 	[estado] [smallint] NOT NULL,
  CONSTRAINT [PK_t_Reserva] PRIMARY KEY CLUSTERED 
@@ -265,25 +241,8 @@ CREATE TABLE [dbo].[t_Reserva](
 GO
 SET ANSI_PADDING OFF
 GO
-/****** Object:  Table [dbo].[t_Cuenta]    Script Date: 05/30/2015 22:26:18 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [dbo].[t_Cuenta](
-	[idCuenta] [int] NOT NULL,
-	[idReserva] [int] NOT NULL,
-	[idServicio] [int] NOT NULL,
-	[cantidad] [int] NOT NULL,
-	[costoUnitario] [decimal](14, 2) NULL,
-	[total] [decimal](18, 2) NULL,
- CONSTRAINT [PK_t_Cuenta_1] PRIMARY KEY CLUSTERED 
-(
-	[idCuenta] ASC
-)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
-) ON [PRIMARY]
-GO
-/****** Object:  Table [dbo].[t_Pasajero]    Script Date: 05/30/2015 22:26:18 ******/
+INSERT [dbo].[t_Reserva] ([idReserva], [idCliente], [idHabitacion], [fechaLlegada], [fechaSalida], [fechaHoraCheckin], [comentarioCheckin], [fechaHoraCheckout], [comentarioCheckout], [codFormaPago], [numeroTarjeta], [titularTarjeta], [mesExpiraTarjeta], [anioExpiraTarjeta], [requerimientosEsp], [observaciones], [estadoCuenta], [estado]) VALUES (1, 1, 1, CAST(0x000066B6005737BC AS DateTime), CAST(0x000066B6005737BC AS DateTime), CAST(0x000066B6005737BC AS DateTime), N'CheckIn sin problemas', CAST(0x0000A4AB0177189C AS DateTime), N'CheckOut sin problemas', N'EF', N'2312312', N'asdasdas', 8, 2015, N'Sin requerimientos', N'', 1, 3)
+/****** Object:  Table [dbo].[t_Pasajero]    Script Date: 06/10/2015 20:15:51 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -304,51 +263,101 @@ CREATE TABLE [dbo].[t_Pasajero](
 GO
 SET ANSI_PADDING OFF
 GO
-/****** Object:  ForeignKey [FK_t_Cliente_t_Pais]    Script Date: 05/30/2015 22:26:18 ******/
+/****** Object:  Table [dbo].[t_Cuenta]    Script Date: 06/10/2015 20:15:51 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[t_Cuenta](
+	[idCuenta] [int] NOT NULL,
+	[idReserva] [int] NOT NULL,
+	[idServicio] [int] NOT NULL,
+	[cantidad] [int] NOT NULL,
+	[costoUnitario] [decimal](14, 2) NULL,
+	[total] [decimal](18, 2) NULL,
+ CONSTRAINT [PK_t_Cuenta_1] PRIMARY KEY CLUSTERED 
+(
+	[idCuenta] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[t_Comprobante]    Script Date: 06/10/2015 20:15:51 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_PADDING ON
+GO
+CREATE TABLE [dbo].[t_Comprobante](
+	[idComprobante] [int] NOT NULL,
+	[idReserva] [int] NOT NULL,
+	[serie] [varchar](5) NOT NULL,
+	[numero] [varchar](7) NOT NULL,
+	[fechaEmision] [datetime] NOT NULL,
+	[importe] [decimal](18, 2) NOT NULL,
+	[igv] [decimal](6, 2) NOT NULL,
+	[importeIgv] [decimal](18, 2) NOT NULL,
+	[importeTotal] [decimal](18, 2) NOT NULL,
+	[estado] [smallint] NOT NULL,
+ CONSTRAINT [PK_t_Comprobante] PRIMARY KEY CLUSTERED 
+(
+	[idComprobante] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+SET ANSI_PADDING OFF
+GO
+/****** Object:  ForeignKey [FK_t_Cliente_t_Pais]    Script Date: 06/10/2015 20:15:51 ******/
 ALTER TABLE [dbo].[t_Cliente]  WITH CHECK ADD  CONSTRAINT [FK_t_Cliente_t_Pais] FOREIGN KEY([idPais])
 REFERENCES [dbo].[t_Pais] ([idPais])
 GO
 ALTER TABLE [dbo].[t_Cliente] CHECK CONSTRAINT [FK_t_Cliente_t_Pais]
 GO
-/****** Object:  ForeignKey [FK_t_Cliente_t_TipoDocumento]    Script Date: 05/30/2015 22:26:18 ******/
+/****** Object:  ForeignKey [FK_t_Cliente_t_TipoDocumento]    Script Date: 06/10/2015 20:15:51 ******/
 ALTER TABLE [dbo].[t_Cliente]  WITH CHECK ADD  CONSTRAINT [FK_t_Cliente_t_TipoDocumento] FOREIGN KEY([idTipoDocumento])
 REFERENCES [dbo].[t_TipoDocumento] ([idTipoDocumento])
 GO
 ALTER TABLE [dbo].[t_Cliente] CHECK CONSTRAINT [FK_t_Cliente_t_TipoDocumento]
 GO
-/****** Object:  ForeignKey [FK_t_Habitacion_t_TipoHabitacion]    Script Date: 05/30/2015 22:26:18 ******/
-ALTER TABLE [dbo].[t_Habitacion]  WITH CHECK ADD  CONSTRAINT [FK_t_Habitacion_t_TipoHabitacion] FOREIGN KEY([idTipoHabitacion])
-REFERENCES [dbo].[t_TipoHabitacion] ([idTipoHabitacion])
+/****** Object:  ForeignKey [FK_t_Comprobante_t_Reserva]    Script Date: 06/10/2015 20:15:51 ******/
+ALTER TABLE [dbo].[t_Comprobante]  WITH CHECK ADD  CONSTRAINT [FK_t_Comprobante_t_Reserva] FOREIGN KEY([idReserva])
+REFERENCES [dbo].[t_Reserva] ([idReserva])
 GO
-ALTER TABLE [dbo].[t_Habitacion] CHECK CONSTRAINT [FK_t_Habitacion_t_TipoHabitacion]
+ALTER TABLE [dbo].[t_Comprobante] CHECK CONSTRAINT [FK_t_Comprobante_t_Reserva]
 GO
-/****** Object:  ForeignKey [FK_t_Reserva_t_Cliente]    Script Date: 05/30/2015 22:26:18 ******/
-ALTER TABLE [dbo].[t_Reserva]  WITH CHECK ADD  CONSTRAINT [FK_t_Reserva_t_Cliente] FOREIGN KEY([idCliente])
-REFERENCES [dbo].[t_Cliente] ([idCliente])
-GO
-ALTER TABLE [dbo].[t_Reserva] CHECK CONSTRAINT [FK_t_Reserva_t_Cliente]
-GO
-/****** Object:  ForeignKey [FK_t_Reserva_t_Habitacion]    Script Date: 05/30/2015 22:26:18 ******/
-ALTER TABLE [dbo].[t_Reserva]  WITH CHECK ADD  CONSTRAINT [FK_t_Reserva_t_Habitacion] FOREIGN KEY([idHabitacion])
-REFERENCES [dbo].[t_Habitacion] ([idHabitacion])
-GO
-ALTER TABLE [dbo].[t_Reserva] CHECK CONSTRAINT [FK_t_Reserva_t_Habitacion]
-GO
-/****** Object:  ForeignKey [FK_t_Cuenta_t_Reserva]    Script Date: 05/30/2015 22:26:18 ******/
+/****** Object:  ForeignKey [FK_t_Cuenta_t_Reserva]    Script Date: 06/10/2015 20:15:51 ******/
 ALTER TABLE [dbo].[t_Cuenta]  WITH CHECK ADD  CONSTRAINT [FK_t_Cuenta_t_Reserva] FOREIGN KEY([idReserva])
 REFERENCES [dbo].[t_Reserva] ([idReserva])
 GO
 ALTER TABLE [dbo].[t_Cuenta] CHECK CONSTRAINT [FK_t_Cuenta_t_Reserva]
 GO
-/****** Object:  ForeignKey [FK_t_Cuenta_t_Servicio]    Script Date: 05/30/2015 22:26:18 ******/
+/****** Object:  ForeignKey [FK_t_Cuenta_t_Servicio]    Script Date: 06/10/2015 20:15:51 ******/
 ALTER TABLE [dbo].[t_Cuenta]  WITH CHECK ADD  CONSTRAINT [FK_t_Cuenta_t_Servicio] FOREIGN KEY([idServicio])
 REFERENCES [dbo].[t_Servicio] ([idServicio])
 GO
 ALTER TABLE [dbo].[t_Cuenta] CHECK CONSTRAINT [FK_t_Cuenta_t_Servicio]
 GO
-/****** Object:  ForeignKey [FK_t_Pasajero_t_Reserva]    Script Date: 05/30/2015 22:26:18 ******/
+/****** Object:  ForeignKey [FK_t_Habitacion_t_TipoHabitacion]    Script Date: 06/10/2015 20:15:51 ******/
+ALTER TABLE [dbo].[t_Habitacion]  WITH CHECK ADD  CONSTRAINT [FK_t_Habitacion_t_TipoHabitacion] FOREIGN KEY([idTipoHabitacion])
+REFERENCES [dbo].[t_TipoHabitacion] ([idTipoHabitacion])
+GO
+ALTER TABLE [dbo].[t_Habitacion] CHECK CONSTRAINT [FK_t_Habitacion_t_TipoHabitacion]
+GO
+/****** Object:  ForeignKey [FK_t_Pasajero_t_Reserva]    Script Date: 06/10/2015 20:15:51 ******/
 ALTER TABLE [dbo].[t_Pasajero]  WITH CHECK ADD  CONSTRAINT [FK_t_Pasajero_t_Reserva] FOREIGN KEY([idReserva])
 REFERENCES [dbo].[t_Reserva] ([idReserva])
 GO
 ALTER TABLE [dbo].[t_Pasajero] CHECK CONSTRAINT [FK_t_Pasajero_t_Reserva]
+GO
+/****** Object:  ForeignKey [FK_t_Reserva_t_Cliente]    Script Date: 06/10/2015 20:15:51 ******/
+ALTER TABLE [dbo].[t_Reserva]  WITH CHECK ADD  CONSTRAINT [FK_t_Reserva_t_Cliente] FOREIGN KEY([idCliente])
+REFERENCES [dbo].[t_Cliente] ([idCliente])
+GO
+ALTER TABLE [dbo].[t_Reserva] CHECK CONSTRAINT [FK_t_Reserva_t_Cliente]
+GO
+/****** Object:  ForeignKey [FK_t_Reserva_t_Habitacion]    Script Date: 06/10/2015 20:15:51 ******/
+ALTER TABLE [dbo].[t_Reserva]  WITH CHECK ADD  CONSTRAINT [FK_t_Reserva_t_Habitacion] FOREIGN KEY([idHabitacion])
+REFERENCES [dbo].[t_Habitacion] ([idHabitacion])
+GO
+ALTER TABLE [dbo].[t_Reserva] CHECK CONSTRAINT [FK_t_Reserva_t_Habitacion]
 GO
