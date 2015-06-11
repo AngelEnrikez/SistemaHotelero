0e88a9a298c40a679baa965b854869066a5ce3b5
ibaraybar <ibaraybard@gmail.com>
2015-06-11 10:04:49 -0500

    Nueva clase de dominio Comprobante

---
+++ Servicios/ServiciosHoteles/Dominio/Comprobante.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace ServiciosHoteles.Dominio
+{
+    [DataContract]
+    public class Comprobante
+    {
+        [DataMember]
+        public int IdComprobante { get; set; }
+        [DataMember]
+        public Reserva Reserva { get; set; }
+        [DataMember]
+        public string Serie { get; set; }
+        [DataMember]
+        public string Numero { get; set; }
+        [DataMember]
+        public DateTime FechaEmision { get; set; }
+        [DataMember]
+        public decimal Importe { get; set; }
+        [DataMember]
+        public decimal Igv { get; set; }
+        [DataMember]
+        public decimal ImporteIgv { get; set; }
+        [DataMember]
+        public decimal ImporteTotal { get; set; }
+        [DataMember]
+        public int Estado { get; set; }
+    }
+}
