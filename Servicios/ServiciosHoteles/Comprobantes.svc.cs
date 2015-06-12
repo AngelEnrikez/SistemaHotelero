5aeb3e9324b2703c4912d75f21c8818e231407a1
ibaraybar <ibaraybard@gmail.com>
2015-06-11 20:05:32 -0500

    servicio comprobantes

---
+++ Servicios/ServiciosHoteles/Comprobantes.svc.cs
@@ -0,0 +1,34 @@
+﻿using ServiciosHoteles.Dominio;
+using ServiciosHoteles.Persistencia;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ServiciosHoteles
+{
+    public class Comprobantes : IComprobantes
+    {
+        private ComprobanteDAO comprobanteDAO = null;
+        private ComprobanteDAO ComprobanteDAO
+        {
+            get
+            {
+                if (comprobanteDAO == null)
+                    comprobanteDAO = new ComprobanteDAO();
+                return comprobanteDAO;
+            }
+        }
+        public Comprobante CrearComprobante(Comprobante comprobanteNuevo)
+        {
+            return ComprobanteDAO.Crear(comprobanteNuevo);
+        }
+
+        public Comprobante ObtenerComprobante(int idComprobante)
+        {
+            return ComprobanteDAO.Obtener(idComprobante);
+        }
+    }
+}
---
+++ Servicios/ServiciosHoteles/IComprobantes.cs
@@ -0,0 +1,19 @@
+﻿using ServiciosHoteles.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ServiciosHoteles
+{
+    [ServiceContract]
+    public interface IComprobantes
+    {
+        [OperationContract]
+        Comprobante CrearComprobante(Comprobante comprobanteNuevo);
+        [OperationContract]
+        Comprobante ObtenerComprobante(int idComprobante);
+    }
+}
